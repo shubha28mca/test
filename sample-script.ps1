f79d7e2153b6fb319c51b780543d986799a747bd
shubha28mca <32094611+shubha28mca@users.noreply.github.com>
2023-08-24 00:10:08 +0530

    Update sample-script.ps1

--- sample-script.ps1
+++ sample-script.ps1
@@ -1,4 +1,10 @@
-$filePath = "C:\FaultTestdummy.txt"
+# Get the current date and time
+$timestamp = Get-Date
+
+# Format the timestamp as a string
+$timestampString = $timestamp.ToString("yyyyMMddHHmmss")
+$fileName = "FaultTestdummy"+$timestampString+".txt"
+$filePath = Join-Path $env:TEMP $fileName
 $content = "This is a test content from fault injection script."
 $content | Set-Content -Path $filePath
 
