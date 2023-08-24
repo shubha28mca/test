26bccd97c054d97a80fdefa20a6fdb5b038c859a
shubha28mca <32094611+shubha28mca@users.noreply.github.com>
2023-08-24 22:59:56 +0530

    Update sample-script.ps1

--- sample-script.ps1
+++ sample-script.ps1
@@ -1,4 +1,5 @@
 # Get the current date and time
+Write-Host "START Script for Fault."
 $timestamp = Get-Date
 
 # Format the timestamp as a string
@@ -8,8 +9,13 @@ $filePath = Join-Path $env:TEMP $fileName
 $content = "This is a test content from fault injection script."
 $content | Set-Content -Path $filePath
 
+$fileItem = Get-Item -Path $filePath 
+Write-Host "Full Path: $($fileItem.FullName)"
+
+
 $processList = Get-Process | Select-Object Name, Id
 
 $processList | ForEach-Object {
     $_.Name + " (PID: " + $_.Id + ")" | Out-File -Append -FilePath $filePath
 }
+Write-Host "End Script for Fault."
